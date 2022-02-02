e6967c25a394c618892e9d3f4e0b048393933f7a
JuanT12 <97958168+JuanT12@users.noreply.github.com>
2022-02-02 17:46:43 -0300

    Add files via upload

---
+++ AustralStudio.js
@@ -0,0 +1,4 @@
+let nombre = prompt("Hola, bienvenido a la pagina oficial de Austral Studios, como te llamas?")
+
+alert("el programador de la pagina ruega una captura para sentirse Bill Gates, ya que tovi no me paga y desvaloriza mi trabajo (ayuda estoy en un sotano y no como nadaaa)")
+
